693fdecaac112c236d534554086c6527b20608ea
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-03-25 20:56:57 +0530

    Added CreatePortal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,26 +1,39 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import "./Modal.css";
+
+
+
+
+
 
 function Modal(props) {
 
 
   return (
-    <div className="blur">
-      <div className="ModalDiv">
-        <div className="ModalDiv-heading">
-          <h1>OOPs!</h1>
-          <p>{props.data.head}</p>
-        </div>
-
-        <div className="ModalDiv-error" >
-          <p>{props.data.error}</p>
-        </div>
-
-        <div className="ModalDiv-btn" >
-          <button onClick={()=>{props.closeModal(false)}} >Close</button>
+    <>
+    {
+      ReactDOM.createPortal(
+        <div className="blur">
+        <div className="ModalDiv">
+          <div className="ModalDiv-heading">
+            <h1>OOPs!</h1>
+            <p>{props.data.head}</p>
+          </div>
+    
+          <div className="ModalDiv-error" >
+            <p>{props.data.error}</p>
+          </div>
+    
+          <div className="ModalDiv-btn" >
+            <button onClick={()=>{props.closeModal(false)}} >Close</button>
+          </div>
         </div>
       </div>
-    </div>
+        
+        , document.querySelector("#error-card"))
+    }
+    </>
   );
 }
 
